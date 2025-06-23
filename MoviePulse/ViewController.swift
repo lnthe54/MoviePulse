0fc890e128c2a238cd7c896bd3c5bd9c9a0bf6b0
TheLN <lengocthe54@gmail.com>
2025-06-23 21:41:52 +0700

    Initial Commit

---
+++ MoviePulse/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoviePulse
+//
+//  Created by The Ngoc on 2025/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
